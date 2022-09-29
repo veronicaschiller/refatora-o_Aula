d192daf7cebfe7f3fe6d8b3c1ffba810a60fbc28
veronicaschiller <veronicaschiller17@gmail.com>
2022-09-29 20:34:03 -0300

    :recycle: refacore the file
    adição de sujestão do prof

--- insulinaRefatorção.js
+++ insulinaRefatorção.js
@@ -12,51 +12,56 @@ let teclado = prompt();
 let option = 0;
 
 const menu = (InsulinaNph, insulinaRapida, InsulinaComp) => {
+    const estaAlterado = TesteDoMomento >= 160 && TesteDoMomento < 240
+    const estaMaisAlterado = TesteDoMomento >= 240 && TesteDoMomento < 260
+    const estaAlto = TesteDoMomento > 260 && TesteDoMomento < 300
+    const estaMuitoAlto = TesteDoMomento > 300 && TesteDoMomento < 400
+    const estaMuitoBaixo = TesteDoMomento < 73
 
     while (option != 6) {
         TesteDoMomento = +teclado("Indice Glicêmico: ")
         console.log(`------------ Insulina para ${TesteDoMomento} de glicose ---------------`);
-        console.log("* 1- 140 <> 160----------------------*");
-        console.log("* 2- 160 <> 240---------------------*");
-        console.log("* 3- 240 <> 260 --------------------*");
-        console.log("* 4- 260 <> 300---------------------*");
-        console.log("* 5- 300<> 400 ---------------------*");
+        console.log("* 1- 140 < > 160----------------------*");
+        console.log("* 2- 160 < > 240---------------------*");
+        console.log("* 3- 240 < > 260 --------------------*");
+        console.log("* 4- 260 < > 300---------------------*");
+        console.log("* 5- 300 < > 400 ---------------------*");
         console.log("* 6- < 73 ----------------------------*");
         console.log("* 7- Sair ----------------------------*");
         console.log("-------------------------------------");
         option = +teclado("Escolha a refeição (Digite '5' para sair): ");
         switch (option) {
             case 1:
-                if (TesteDoMomento >= 140 && TesteDoMomento < 160) {
+                if (estaLevementeAlterado) {
                     console.log(`Sua primeira dosagem é: ${InsulinaComp[0] + insulinaRapida[0]}`)
                     console.log(`Sua dosagem total é:  ${InsulinaNph[0] + insulinaRapida[0] + InsulinaComp[0]}`);
-                } 
-                case 2: if (TesteDoMomento >= 160 && TesteDoMomento < 240) {
-                    console.log(`Sua primeira dosagem é: ${InsulinaComp[1] + insulinaRapida[0]}`)
-                    console.log(`Sua dosagem total é:  ${InsulinaNph[0] + insulinaRapida[0] + InsulinaComp[1]}`);
-                } 
-                case 3:
-                     if (TesteDoMomento >= 240 && TesteDoMomento < 260) {
+                } T
+            case 2: if (estaAlterado) {
+                console.log(`Sua primeira dosagem é: ${InsulinaComp[1] + insulinaRapida[0]}`)
+                console.log(`Sua dosagem total é:  ${InsulinaNph[0] + insulinaRapida[0] + InsulinaComp[1]}`);
+            }
+            case 3:
+                if (estaMaisAlterado) {
                     console.log(`Sua primeira dosagem é: ${InsulinaComp[2] + insulinaRapida[0]}`)
                     console.log(`Sua dosagem total é:  ${InsulinaNph[0] + insulinaRapida[0] + InsulinaComp[2]}`);
-                } 
-                case 4:
-                     if (TesteDoMomento > 260 && TesteDoMomento < 300) {
+                }
+            case 4:
+                if (estaAlto) {
                     console.log(`Sua primeira dosagem é: ${InsulinaComp[3] + insulinaRapida[0]}`)
                     console.log(`Sua dosagem total é:  ${InsulinaNph[0] + insulinaRapida[0] + InsulinaComp[3]}`);
                 }
-                case 5:
-                     if (TesteDoMomento > 300 && TesteDoMomento < 400) {
+            case 5:
+                if (estaMuitoAlto) {
                     console.log(`Sua primeira dosagem é: ${InsulinaComp[5] + insulinaRapida[0]}`)
                     console.log(`Sua dosagem total é:  ${InsulinaNph[0] + insulinaRapida[0] + InsulinaComp[5]}`);
                 }
-                case 6: 
-                
-                    if (TesteDoMomento <73) {
-                        console.log(`Sua primeira dosagem é: ${  insulinaRapida[0] - 1}`)
-                        console.log(`Sua dosagem total é:  ${InsulinaNph[0] + insulinaRapida[0] - 1}`);
-                    } 
-                case 7: 
+            case 6:
+
+                if (estaMuitoBaixo) {
+                    console.log(`Sua primeira dosagem é: ${insulinaRapida[0] - 1}`)
+                    console.log(`Sua dosagem total é:  ${InsulinaNph[0] + insulinaRapida[0] - 1}`);
+                }
+            case 7:
                 console.log("ate a proxima !")
                 break;
             default:
