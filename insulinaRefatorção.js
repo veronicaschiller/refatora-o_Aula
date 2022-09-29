c81bbf5cfb18df7ad5e63f692d942bfd0925c612
veronicaschiller <veronicaschiller17@gmail.com>
2022-09-29 20:01:55 -0300

    :recycle: refactor the file
    troca de nomes pra normas do clean code

--- insulinaRefatorção.js
+++ insulinaRefatorção.js
@@ -2,16 +2,16 @@ const prompt = require("prompt-sync");
 
 let TesteDoMomento = Number(0);
 
-const Nph = [14, 14, 14];
+const InsulinaNph = [14, 14, 14];
 
-const Rapida = [7, 7, 8, 4];
+const insulinaRapida = [7, 7, 8, 4];
 
-const comp = [1, 2, 3, 4, 5];
+const InsulinaComp = [1, 2, 3, 4, 5];
 
 let teclado = prompt();
 let option = 0;
 
-const menu = (Nph, Rapida, comp) => {
+const menu = (InsulinaNph, insulinaRapida, InsulinaComp) => {
 
     while (option != 6) {
         TesteDoMomento = +teclado("Indice Glicêmico: ")
@@ -28,33 +28,33 @@ const menu = (Nph, Rapida, comp) => {
         switch (option) {
             case 1:
                 if (TesteDoMomento >= 140 && TesteDoMomento < 160) {
-                    console.log(`Sua primeira dosagem é: ${comp[0] + Rapida[0]}`)
-                    console.log(`Sua dosagem total é:  ${Nph[0] + Rapida[0] + comp[0]}`);
+                    console.log(`Sua primeira dosagem é: ${InsulinaComp[0] + insulinaRapida[0]}`)
+                    console.log(`Sua dosagem total é:  ${InsulinaNph[0] + insulinaRapida[0] + InsulinaComp[0]}`);
                 } 
                 case 2: if (TesteDoMomento >= 160 && TesteDoMomento < 240) {
-                    console.log(`Sua primeira dosagem é: ${comp[1] + Rapida[0]}`)
-                    console.log(`Sua dosagem total é:  ${Nph[0] + Rapida[0] + comp[1]}`);
+                    console.log(`Sua primeira dosagem é: ${InsulinaComp[1] + insulinaRapida[0]}`)
+                    console.log(`Sua dosagem total é:  ${InsulinaNph[0] + insulinaRapida[0] + InsulinaComp[1]}`);
                 } 
                 case 3:
                      if (TesteDoMomento >= 240 && TesteDoMomento < 260) {
-                    console.log(`Sua primeira dosagem é: ${comp[2] + Rapida[0]}`)
-                    console.log(`Sua dosagem total é:  ${Nph[0] + Rapida[0] + comp[2]}`);
+                    console.log(`Sua primeira dosagem é: ${InsulinaComp[2] + insulinaRapida[0]}`)
+                    console.log(`Sua dosagem total é:  ${InsulinaNph[0] + insulinaRapida[0] + InsulinaComp[2]}`);
                 } 
                 case 4:
                      if (TesteDoMomento > 260 && TesteDoMomento < 300) {
-                    console.log(`Sua primeira dosagem é: ${comp[3] + Rapida[0]}`)
-                    console.log(`Sua dosagem total é:  ${Nph[0] + Rapida[0] + comp[3]}`);
+                    console.log(`Sua primeira dosagem é: ${InsulinaComp[3] + insulinaRapida[0]}`)
+                    console.log(`Sua dosagem total é:  ${InsulinaNph[0] + insulinaRapida[0] + InsulinaComp[3]}`);
                 }
                 case 5:
                      if (TesteDoMomento > 300 && TesteDoMomento < 400) {
-                    console.log(`Sua primeira dosagem é: ${comp[5] + Rapida[0]}`)
-                    console.log(`Sua dosagem total é:  ${Nph[0] + Rapida[0] + comp[5]}`);
+                    console.log(`Sua primeira dosagem é: ${InsulinaComp[5] + insulinaRapida[0]}`)
+                    console.log(`Sua dosagem total é:  ${InsulinaNph[0] + insulinaRapida[0] + InsulinaComp[5]}`);
                 }
                 case 6: 
                 
                     if (TesteDoMomento <73) {
-                        console.log(`Sua primeira dosagem é: ${  Rapida[0] - 1}`)
-                        console.log(`Sua dosagem total é:  ${Nph[0] + Rapida[0] - 1}`);
+                        console.log(`Sua primeira dosagem é: ${  insulinaRapida[0] - 1}`)
+                        console.log(`Sua dosagem total é:  ${InsulinaNph[0] + insulinaRapida[0] - 1}`);
                     } 
                 case 7: 
                 console.log("ate a proxima !")
@@ -64,4 +64,4 @@ const menu = (Nph, Rapida, comp) => {
         }
     }
 }
-menu(Nph, Rapida, comp)
+menu(InsulinaNph, insulinaRapida, InsulinaComp)
